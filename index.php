60c24710686a53b8138e284aeb2e7667ccbd9148
Dylan Reynolds <c2022508@hallam.shu.ac.uk>
2023-11-15 21:14:35 +0000

    start

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+include("includes/connection.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/mobile.css">
+    <link rel="stylesheet" media="only screen and (min-width: 720px)" href="css/desktop.css">
+    <title>Home</title>
+</head>
+
+<body>
+    <div id="container" class="container">
+        <header id="header" class="header">
+            <!-- something for the drop down menu -->
+            <div id="logo" class="logo">
+                <a href="index.php"><img src="" alt="ModernFit Logo"></a>
+            </div>
+            <nav id="header-nav">
+                <ul>
+                    <li><a href="registration.php">Register</a></li>
+                    <li><a href="signIn.php">Sign In</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div id="main">
+            <div id="opening-times-container" class="opening-times-container">
+                <ul>
+                    <li>Monday 9.00 - 21.00</li>
+                    <li>Tuesday 9.00 - 21.00</li>
+                    <li>Wednesday 9.00 - 21.00</li>
+                    <li>Thursday 9.00 - 21.00</li>
+                    <li>Friday 9.00 - 21.00</li>
+                    <li>Saturday 9.00 - 21.00</li>
+                    <li>Sunday 9.00 - 21.00</li>
+                </ul>
+            </div>
+            <div id="services-container" class="services-container">
+                <h1>Services</h1>
+                <ul>
+                    <li>Tailored programs</li>
+                    <li>Nutritioanl guidance</li>
+                    <li>Progress monitoring</li>
+                </ul>
+            </div>
+            <div id="facilities-container" class="facilities-container">
+                <h1>Facilities</h1>
+                <ul>
+                    <li>Gym facilities</li>
+                    <li>Specialised staff</li>
+                </ul>
+            </div>
+        </div>
+        <footer></footer>
+    </div>
+</body>
+
+</html>
---
+++ servicesAndFacilites.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+include("includes/connection.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/mobile.css">
+  <link rel="stylesheet" media="only screen and (min-width: 720px)" href="css/desktop.css">
+  <title>Services and Facilities</title>
+</head>
+
+<body>
+  <div id="container" class="container">
+    <header id="header" class="header">
+      <!-- something for the drop down menu -->
+      <div id="logo" class="logo">
+        <a href="index.php"><img src="" alt="ModernFit Logo"></a>
+      </div>
+      <nav id="header-nav">
+        <ul>
+          <li><a href="includes/signOut.php">Sign Out</a></li>
+        </ul>
+      </nav>
+    </header>
+    <div id="main">
+      <div id="services-container" class="services-container">
+        <h1>Services</h1>
+        <ul>
+          <li>Monitor your progress with the progress tracker, everything is put into easy-to-read graphs/charts.</li>
+          <li>We offer tailored programs catered to each members individual needs, these programs are made by our team of personal trainers.</li>
+          <li>The team of personal trainers also offer nutritional guidance, this caters to the members goals, whether they eat meat or are vegan.</li>
+        </ul>
+      </div>
+      <div id="facilities-container" class="facilities-container">
+        <h1>Facilities</h1>
+        <ul>
+          <li>Indoor heated swimming pool for exercise in the water.</li>
+          <li>In our sauna and steam room, unwind and rest.</li>
+          <li>A variety of cardio machines are available, including treadmills, elliptical trainers, and stationary bikes.</li>
+        </ul>
+      </div>
+    </div>
+    <footer></footer>
+  </div>
+</body>
+
+</html>
