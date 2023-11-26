e7c0d4a4c58d29f366e64b5727d211ec7c7b6ca9
Win3065213 <AungNaing.N.Win@student.shu.ac.uk>
2023-11-26 22:13:48 +0000

    made changes to include 'memberHeader.php'

--- nutritionalInfo.php
+++ nutritionalInfo.php
@@ -1,5 +1,6 @@
 <?php
-    include 'includes/autoloader.php'
+    include 'includes/autoloader.php';
+    include 'includes/memberHeader.php';
 ?>
 
 <!DOCTYPE html>
@@ -15,17 +16,7 @@
 
 <body>
   <div id="container" class="container">
-    <header id="header" class="header">
-      <!-- something for the drop down menu -->
-      <div id="logo" class="logo">
-        <a href="index.php"><img src="" alt="ModernFit Logo"></a>
-      </div>
-      <nav id="header-nav">
-        <ul>
-          <li><a href="includes/signOut.php">Sign Out</a></li>
-        </ul>
-      </nav>
-    </header>
+    
     <div id="main">
 
     </div>
