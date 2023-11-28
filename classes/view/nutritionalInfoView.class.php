84d6dba15699ba8907dc450d06561f0bc67d5fc0
Win3065213 <AungNaing.N.Win@student.shu.ac.uk>
2023-11-28 16:32:43 +0000

    edited error in 'nutritionalInfo.php'

--- classes/view/nutritionalInfoView.class.php
+++ classes/view/nutritionalInfoView.class.php
@@ -3,7 +3,7 @@
 class NI_View extends NI_Model{
 
   public function showIngredients($ingredient){
-    $ingredient_data = $this->getUserId($ingredient);
+    $ingredient_data = $this->getIngredients($ingredient);
     $obj = $ingredient_data->fetch();
     return $obj;
   }
