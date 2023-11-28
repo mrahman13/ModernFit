9c8f3f1660f0e474c4249abd824819f53b6a1430
Win3065213 <AungNaing.N.Win@student.shu.ac.uk>
2023-11-28 16:28:52 +0000

    added functions in 'nutritionalInfo.php'

--- nutritionalInfo.php
+++ nutritionalInfo.php
@@ -1,6 +1,11 @@
 <?php
     include 'includes/autoloader.php';
     include 'includes/memberHeader.php';
+    //might need get method or direct connection
+    
+    $data = new NI_View();
+    $integ_data = $data->showIngredients();
+    
 ?>
 
 <!DOCTYPE html>
@@ -15,10 +20,41 @@
 </head>
 
 <body>
-  <div id="container" class="container">
-    
     <div id="main">
 
+
+      <div class="search-box">
+        <form class="" action="nutritionalInfo.php">
+          <input type="text" class="" placeholder="Search Ingredient..." name="key" value="<?php isset($_GET['keyword']) ? $_GET['keyword'] : '' ?>">
+          <button type="submit" class="">Search</button>
+        </form>
+      </div>
+
+      <div class="ingredient-body">
+        <div class="ingredient">
+          
+          <?php
+          
+          foreach($integ_data as $row) {?>
+            <div class="responsive-row">
+              <div class="container set-padding" id="integ">
+
+                <div class="image">
+                  <img class="img-responsive" src="/img/<?php $row['image'] ?>">
+                </div>
+
+                <div class="info">
+                  <p id='calorie'><?php echo "Calorie Count: ".$row['calories'] ?></p>
+                  <p id='protein'><?php echo "Protein: ".$row['protein'] ?></p>
+                  <p id='carbohydrates'><?php echo "Carbohydrates: ".$row['carbohydrates'] ?></p>
+                  <p id='fat'><?php echo "Fat: ".$row['fat'] ?></p>
+                </div>
+              </div>
+            </div>
+          <?php } ?>
+
+        </div>
+      </div>
     </div>
     <footer></footer>
   </div>
