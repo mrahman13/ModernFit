9168d6c8f75f5f4448515b7ce7cf0f1826577963
Win3065213 <AungNaing.N.Win@student.shu.ac.uk>
2023-11-30 13:50:38 +0000

    adjustment for routing system

--- modernfit.php
+++ modernfit.php
@@ -12,10 +12,6 @@ switch ($request) {
         require __DIR__ . $viewDir . 'users.php';
         break;*/
 
-    case '/nutrition':
-        require __DIR__ . '/nutritionalInfo.php';
-        break;
-
     default:
         http_response_code(404);
         require __DIR__ . '/404.php';
