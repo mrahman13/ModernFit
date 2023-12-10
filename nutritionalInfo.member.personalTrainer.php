6add59b7150463f5c05144272bfdd3a8ff2468c1
Win3065213 <AungNaing.N.Win@student.shu.ac.uk>
2023-12-10 16:21:58 +0000

    spelling correction, web design

--- nutritionalInfo.member.personalTrainer.php
+++ nutritionalInfo.member.personalTrainer.php
@@ -13,7 +13,7 @@
       $key = '';
     }
     $data = new NI_View();
-    $integ_data = $data->showIngredients($key);
+    $ingre_data = $data->showIngredients($key);
     
 ?>
 
@@ -23,43 +23,52 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/mobile.css">
-  <link rel="stylesheet" media="only screen and (min-width: 720px)" href="css/desktop.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="../css/mobile.css">
+  <link rel="stylesheet" media="only screen and (min-width: 720px)" href="../css/desktop.css">
   <title>Nutritional Info</title>
+  
 </head>
 
 <body>
     <div id="main">
 
 
-      <div class="search-box">
-        <form class="" action="" method="post">
-          <input type="text" class="" placeholder="Search Ingredient..." name="key" value="<?php isset($_GET['keyword']) ? $_GET['keyword'] : '' ?>">
-          <button name="search" type="submit" class="">Search</button>
+      <div class="container mx-auto my-2 p-0" id="search-box">
+        <form class="d-flex" method="post">
+          <input type="text" class="form-control w-100 border-3 me-2" placeholder="Search Ingredient..." name="key" value="<?php isset($_GET['keyword']) ? $_GET['keyword'] : '' ?>">
+          <button name="search" type="submit" class="btn btn-outline-warning border-3">Search</button>
         </form>
       </div>
 
-      <div class="ingredient-body">
-        <div class="ingredient">
+      <div class="container p-1 mx-auto" id="ingre-body"> <!-- border -->
+        <div class="row mx-auto" id="ingredient-row"> <!-- border -->
           
           <?php
           
-          foreach($integ_data as $row) {?>
-            <div class="responsive-row">
-              <div class="container set-padding" id="integ">
-                <div class="image">
-                  <img style="width:500px;height:300px;" class="img-responsive" src="../img/<?php echo $row['image'] ?>">
-                </div>
+          foreach($ingre_data as $row) {?>
+            
+            <div class="col-md-6 col-sm-12 p-1 rounded-3" id="ingre"><!-- border border-4 border-danger -->
+              <div class="container text-center">
+                <div class="row py-2"> <!-- border border-4 border-warning -->
+                  <div class="col-xl-6 col-sm-12 m-auto" id="image">
+                    <img class="img-responsive" src="../img/<?php echo $row['image'] ?>" draggable="false">
+                  </div>
 
-                <div class="info">
-                  <p id='ingredient_name'><?php echo "Name: ".$row['ingredient_name'] ?></p>
-                  <p id='calorie'><?php echo "Calorie Count: ".$row['calories'] ?></p>
-                  <p id='protein'><?php echo "Protein: ".$row['protein'] ?></p>
-                  <p id='carbohydrates'><?php echo "Carbohydrates: ".$row['carbohydrates'] ?></p>
-                  <p id='fat'><?php echo "Fat: ".$row['fat'] ?></p>
+                  <div class="col-xl-6 col-sm-12" id="info">
+                    <div class="container px-0 py-1">
+                      <p id='ingredient_name'><b>Name: </b><?php echo $row['ingredient_name'] ?></p>
+                      <p id='ingredient_type'><b>Type: </b><?php echo $row['food_type'] ?></p>
+                      <p id='calorie'><b>Calorie Count: </b><?php echo $row['calories'] ?></p>
+                      <p id='protein'><b>Protein: </b><?php echo $row['protein'] ?></p>
+                      <p id='carbohydrates'><b>Carbohydrates: </b><?php echo $row['carbohydrates'] ?></p>
+                      <p id='fat'><b>Fat: </b><?php echo $row['fat'] ?></p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
+            
           <?php } ?>
 
         </div>
