df662983330cf35fa5964fdeca4425c3cb5d87fe
Win3065213 <AungNaing.N.Win@student.shu.ac.uk>
2023-12-12 15:22:14 +0000

    Completed 404.php

--- 404.php
+++ 404.php
@@ -1,7 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-    <body>
-        <h1>404</h1>
-        <p>Page not found.</p>
-    </body>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="../css/desktop.css">
+  <style>
+    body {
+        width: 100%;
+        height: 100vh;
+        margin: 0;
+    }
+    .content{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+    }
+  </style>
+  <title>404</title>
+</head>
+<body>
+    <div class="content d-flex flex-column">
+        <div class="d-flex">
+            <div class="display-1 text-warning px-2 mb-0"><b><b>404</b></b></div>
+            <div class="h5 align-self-end">Page not found.</div>
+        </div>
+        <button class="btn btn-link text-warning" onclick="history.back()">Go Back</button>
+    </div>
+    <!-- <div class="container">
+        <div class="d-flex w-100 justify-content-center">
+            <div class="h1 border ">404</div>
+            <div class="align-self-end border">Page not found.</div>
+        </div>
+        <a onclick="history.back()">Go Back</a>
+    </div> -->
+</body>
 </html>
