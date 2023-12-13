f1065f9a2df554cb9836318c81ef5e9cb8caca63
mrahman13 <116874917+mrahman13@users.noreply.github.com>
2023-12-13 00:52:07 +0000

    Email function - using public function on connection

---
+++ sendEmail.manager.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Send Email</title>
+</head>
+<body>
+    <form xlass = "" action="send.php" method="post">
+    Subject <input type = "text" name ="subject" value = ""> <br>
+    Message <input type = "text" name ="message" value = ""> <br>
+    <button type = "submit" name = "send"> Send</button>
+    </form>
+
+</body>
+</html>
