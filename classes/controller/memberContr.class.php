a454b5d497131b4e458a61fac3b9b9971e9a99b5
Dylan Reynolds <c2022508@hallam.shu.ac.uk>
2023-12-20 15:24:22 +0000

    error message

--- classes/controller/memberContr.class.php
+++ classes/controller/memberContr.class.php
@@ -5,7 +5,7 @@ class memberContr extends memberModel{
   public function createMember($first_name, $last_name, $profile_picture, $date_joined, $user_id){
     $this->setMember($first_name, $last_name, $profile_picture, $date_joined, $user_id);
   }
-  // public function createMember($first_name, $last_name, $profile_picture, $date_joined){
-  //   $this->setMember($first_name, $last_name, $profile_picture, $date_joined);
-  // }
+  public function searchMember($member) {
+    return $this->searchMemberName($member);
+    }
 }
