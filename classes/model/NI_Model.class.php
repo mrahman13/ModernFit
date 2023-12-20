18f967dc6be7502054b83a451bc2f58238ecaa2b
Win3065213 <AungNaing.N.Win@student.shu.ac.uk>
2023-12-20 18:06:37 +0000

    NI_Model changed

--- classes/model/NI_Model.class.php
+++ classes/model/NI_Model.class.php
@@ -4,15 +4,16 @@ class NI_Model extends dbConnection
 {
   protected function getIngredients($ingredient = '')
   {
-    //what aung did originally
-    $sql = "SELECT * from ingredient where ingredient_name like '%$ingredient%'";
-    $ingredient_data = $this->connect()->query($sql);
-    return $ingredient_data;
-    
-    //my attempt to do the same with prepare statemtn
-    // $sql = "SELECT * from ingredient where ingredient_name like %?% ORDER BY ingredient_id DESC";
-    // $ingredient_data = $this->connect()->prepare($sql);
-    // $ingredient_data->execute([$ingredient]);
+    //what I did originally
+    // $sql = "SELECT * from ingredient where ingredient_name like '%$ingredient%'";
+    // $ingredient_data = $this->connect()->query($sql);
     // return $ingredient_data;
+    
+    $input = '%' . $ingredient . '%';
+    $sql = "SELECT * from ingredient where ingredient_name like :ingre";
+    $stmt = $this->connect()->prepare($sql);
+    $stmt->bindParam(':ingre',$input);
+    $stmt->execute();
+    return $stmt;
   }
 }
