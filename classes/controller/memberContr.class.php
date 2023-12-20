ffef20cb5299c4874c6c8ba0ad740861480e9c2a
Dylan Reynolds <c2022508@hallam.shu.ac.uk>
2023-12-20 16:17:48 +0000

    member goals, aungs failed prepared statement

--- classes/controller/memberContr.class.php
+++ classes/controller/memberContr.class.php
@@ -2,8 +2,8 @@
 
 class memberContr extends memberModel{
 
-  public function createMember($first_name, $last_name, $profile_picture, $date_joined, $user_id){
-    $this->setMember($first_name, $last_name, $profile_picture, $date_joined, $user_id);
+  public function createMember($first_name, $last_name, $profile_picture, $goals, $date_joined, $user_id){
+    $this->setMember($first_name, $last_name, $profile_picture, $goals, $date_joined, $user_id);
   }
   public function searchMember($member) {
     return $this->searchMemberName($member);
