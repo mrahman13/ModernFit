7375d790ea0e581768576b48975f2782013f8c5d
Win3065213 <AungNaing.N.Win@student.shu.ac.uk>
2023-12-24 17:44:39 +0000

    Minor fixes

--- managerHomepage.php
+++ managerHomepage.php
@@ -13,8 +13,6 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/mobile.css">
-  <link rel="stylesheet" media="only screen and (min-width: 720px)" href="css/desktop.css">
   <title>Manager Home</title>
 </head>
 
