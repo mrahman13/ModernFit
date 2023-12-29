1fc23015cfef9a242f503b30f059ad541f49abb3
Win3065213 <AungNaing.N.Win@student.shu.ac.uk>
2023-12-29 10:29:24 +0000

    send email to members web page design completed

--- sendEmailAdmin.php
+++ sendEmailAdmin.php
@@ -16,11 +16,15 @@ include 'includes/adminHeader.php';
 </head>
 
 <body>
-    <form class="" action="" method="post">
-        Subject <input type="text" name="subject" value=""> <br>
-        Message <input type="text" name="message" value=""> <br>
-        <button type="submit" name="send"> Send</button>
-    </form>
+    <div id="container" class="mx-sm-4 mx-xl-5 px-2 px-sm-3 px-xl-5">
+
+    <div id="main">
+        <div class="h1 text-warning py-2">Send email to all members</div>
+        <form class="row content mx-1 gy-2" method="post">
+            <input class="form-control border-3" type="text" name="subject" placeholder="Subject:" required>
+            <input class="form-control border-3" type="text" name="message" placeholder="Message:" required>
+            <input class="btn btn-warning" type="submit" name="send">
+        </form>
     <?php
     if (isset($_POST['send'])) {
         $subject = $_POST['subject'];
