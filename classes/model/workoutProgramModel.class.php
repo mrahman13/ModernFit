d65b467dd69fc625932c067ac835e31213d79938
Dylan Reynolds <c2022508@hallam.shu.ac.uk>
2024-01-03 17:18:21 +0000

    show meal in order of day

--- classes/model/workoutProgramModel.class.php
+++ classes/model/workoutProgramModel.class.php
@@ -4,14 +4,32 @@ class workoutProgramModel extends dbConnection
 {
   protected function getWorkoutProgramByMember($member_id)
   {
-    $query = "SELECT * from workout_program WHERE member_id = ?";
+    $query = "SELECT * from workout_program WHERE member_id = ? ORDER BY 
+    CASE
+      WHEN workout_day = 'Monday' THEN 1
+      WHEN workout_day = 'Tuesday' THEN 2
+      WHEN workout_day = 'Wednesday' THEN 3
+      WHEN workout_day = 'Thursday' THEN 4
+      WHEN workout_day = 'Friday' THEN 5
+      WHEN workout_day = 'Saturday' THEN 6
+      WHEN workout_day = 'Sunday' THEN 7
+    END ASC";
     $stmt = $this->connect()->prepare($query);
     $stmt->execute([$member_id]);
     return $stmt;
   }
   protected function getWorkoutProgramByPersonalTrainer($personal_trainer_id)
   {
-    $query = "SELECT * from workout_program WHERE personal_trainer_id = ?";
+    $query = "SELECT * from workout_program WHERE personal_trainer_id = ? ORDER BY 
+    CASE
+      WHEN workout_day = 'Monday' THEN 1
+      WHEN workout_day = 'Tuesday' THEN 2
+      WHEN workout_day = 'Wednesday' THEN 3
+      WHEN workout_day = 'Thursday' THEN 4
+      WHEN workout_day = 'Friday' THEN 5
+      WHEN workout_day = 'Saturday' THEN 6
+      WHEN workout_day = 'Sunday' THEN 7
+    END ASC";
     $stmt = $this->connect()->prepare($query);
     $stmt->execute([$personal_trainer_id]);
     return $stmt;
@@ -24,43 +42,41 @@ class workoutProgramModel extends dbConnection
     $count = $stmt->fetchColumn();
     return $count;
   }
-  protected function SaveWorkoutProgram($workout_name, $notes, $workout_day, $excercises,$member_id,$personal_trainer_id)
-  {
-    {
+  protected function SaveWorkoutProgram($workout_name, $notes, $workout_day, $excercises, $member_id, $personal_trainer_id)
+  { {
       if (!$this->ValidateInput($workout_name, $notes, $workout_day, $excercises)) {
-        return; 
-    }
+        return;
+      }
 
-    $query = "INSERT INTO workout_program (workout_name, notes, workout_day,exercises,member_id, personal_trainer_id) VALUES (?, ?, ?, ?,?,?)";
-    $stmt = $this->connect()->prepare($query);
-    $success = $stmt->execute([$workout_name, $notes, $workout_day, $excercises, $member_id, $personal_trainer_id]);
-    if ($success) {
-    echo "<div class='fs-5 text-warning text-center p-2'>Form has been submitted successfully!</div>";
-  } else {
-      echo "<div class='fs-5 text-warning text-center p-2'>Error: form unable to be submitted.</div>";
+      $query = "INSERT INTO workout_program (workout_name, notes, workout_day,exercises,member_id, personal_trainer_id) VALUES (?, ?, ?, ?,?,?)";
+      $stmt = $this->connect()->prepare($query);
+      $success = $stmt->execute([$workout_name, $notes, $workout_day, $excercises, $member_id, $personal_trainer_id]);
+      if ($success) {
+        echo "<div class='fs-5 text-warning text-center p-2'>Form has been submitted successfully!</div>";
+      } else {
+        echo "<div class='fs-5 text-warning text-center p-2'>Error: form unable to be submitted.</div>";
+      }
+    }
   }
-  }
-}
 
   protected function ValidateInput($workout_name, $notes, $workout_day, $excercises)
   {
     $errors = [];
 
     if (!preg_match("/^[a-zA-Z]+$/", $workout_day)) {
-        $errors[] = "Workout Day should only contain letters.";
-      }
-  
-      if (!empty($errors)) {
-          foreach ($errors as $error) {
-              echo $error;
-          }
+      $errors[] = "Workout Day should only contain letters.";
+    }
 
-          return false; 
+    if (!empty($errors)) {
+      foreach ($errors as $error) {
+        echo $error;
       }
-  
-      return true;
+
+      return false;
+    }
+
+    return true;
   }
-  
 }
 //https://stackoverflow.com/questions/19010269/preg-match-false-results
 //Reference I used to validate only text is being inputted.
